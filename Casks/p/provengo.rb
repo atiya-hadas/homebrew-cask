c54203dc834458ca862f52db5080c2f3d2a6eb03
hadas <atiya.hadas@gmail.com>
2023-09-03 15:07:53 +0300

    provengo 0.7.5

---
+++ Casks/p/provengo.rb
@@ -0,0 +1,23 @@
+cask "provengo" do
+  version "0.7.5"
+  sha256 "629d216ac922c9a531cec8a4d604c509b13b1a2ad3f4b9257218f0034ba592f5"
+
+  url "https://downloads.provengo.tech/installer/provengo-#{version}.zip"
+  name "provengo"
+  desc "Model-based testing and automation system"
+  homepage "https://provengo.tech/"
+  
+  depends_on formula: [
+    "graphviz",
+    "openjdk"
+  ]
+
+  app "Provengo-2023-07-20.uber.jar"
+  binary "provengo"
+  
+  caveats do
+    license "https://provengo.tech/?page_id=169"
+    depends_on_java "11+"
+  end
+  
+end
